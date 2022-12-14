48174fa4af57ea2653670746fbfb61ff6aa6b17c
Sharyy Ivan <sharyy.1990@mail.ru>
2022-12-14 14:48:32 +0700

    fit: add page 404

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,17 @@
+export const PageNotFound = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h2>404</h2>
+      <p>Page not found!</p>
+    </div>
+  );
+};
