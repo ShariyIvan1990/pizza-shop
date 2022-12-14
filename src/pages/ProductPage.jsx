f8d77426f4664cdad4cb0d2d66f3f2d7d0fd1a48
Sharyy Ivan <sharyy.1990@mail.ru>
2022-12-14 14:47:21 +0700

    fit: add products page

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,11 @@
+import { HeaderApp } from "../components/HeaderApp";
+import { Product } from "../components/Product";
+
+export const ProductPage = () => {
+  return (
+    <>
+      <HeaderApp butonBack cart />
+      <Product />
+    </>
+  );
+};
