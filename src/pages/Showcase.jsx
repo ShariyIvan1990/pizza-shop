72d829ede915410b5cecabfcbbf530992ae79e24
Sharyy Ivan <sharyy.1990@mail.ru>
2022-12-14 11:27:11 +0700

    fit: add Showcase page

---
+++ src/pages/Showcase.jsx
@@ -0,0 +1,11 @@
+import { HeaderApp } from "../components/HeaderApp";
+import { Cards } from "../components/Cards";
+
+export const Showcase = () => {
+  return (
+    <>
+      <HeaderApp cart title="наша продукция" />
+      <Cards />
+    </>
+  );
+};
