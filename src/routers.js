23b193f40ae0d4f71d327a2eb4a4871ffcca2be4
Sharyy Ivan <sharyy.1990@mail.ru>
2022-12-15 16:09:59 +0700

    fit: add routers

---
+++ src/routers.js
@@ -0,0 +1,42 @@
+import { Cart } from "./pages/Cart";
+import { Login } from "./components/Login";
+import { Showcase } from "./pages/Showcase";
+import { ProductPage } from "./pages/ProductPage";
+import { PageNotFound } from "./pages/PageNotFound";
+import { RedirectAuth } from "./components/hoc/RedirectAuth";
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    errorElement: <PageNotFound />,
+  },
+
+  {
+    path: "cart",
+    element: (
+      <RedirectAuth>
+        <Cart />
+      </RedirectAuth>
+    ),
+  },
+  {
+    path: "product",
+    element: (
+      <RedirectAuth>
+        <Showcase />
+      </RedirectAuth>
+    ),
+    errorElement: <PageNotFound />,
+  },
+  {
+    path: "product/:id",
+    element: (
+      <RedirectAuth>
+        <ProductPage />
+      </RedirectAuth>
+    ),
+    errorElement: <PageNotFound />,
+  },
+]);
