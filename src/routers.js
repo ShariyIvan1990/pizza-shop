2204d1465d50565f8d4f48043af10f570d072ff4
Sharyy Ivan <sharyy.1990@mail.ru>
2022-12-15 17:40:40 +0700

    fix: add cart basename

--- src/routers.js
+++ src/routers.js
@@ -6,37 +6,40 @@ import { PageNotFound } from "./pages/PageNotFound";
 import { RedirectAuth } from "./components/hoc/RedirectAuth";
 import { createBrowserRouter } from "react-router-dom";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-    errorElement: <PageNotFound />,
-  },
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Login />,
+      errorElement: <PageNotFound />,
+    },
 
-  {
-    path: "cart",
-    element: (
-      <RedirectAuth>
-        <Cart />
-      </RedirectAuth>
-    ),
-  },
-  {
-    path: "product",
-    element: (
-      <RedirectAuth>
-        <Showcase />
-      </RedirectAuth>
-    ),
-    errorElement: <PageNotFound />,
-  },
-  {
-    path: "product/:id",
-    element: (
-      <RedirectAuth>
-        <ProductPage />
-      </RedirectAuth>
-    ),
-    errorElement: <PageNotFound />,
-  },
-]);
+    {
+      path: "cart",
+      element: (
+        <RedirectAuth>
+          <Cart />
+        </RedirectAuth>
+      ),
+    },
+    {
+      path: "product",
+      element: (
+        <RedirectAuth>
+          <Showcase />
+        </RedirectAuth>
+      ),
+      errorElement: <PageNotFound />,
+    },
+    {
+      path: "product/:id",
+      element: (
+        <RedirectAuth>
+          <ProductPage />
+        </RedirectAuth>
+      ),
+      errorElement: <PageNotFound />,
+    },
+  ],
+  { basename: "/pizza-shop" }
+);
